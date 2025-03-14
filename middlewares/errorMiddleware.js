07a7b9a980c3a97db8e85aebee9dd4a9c95ca1fb
SantoshThapaa <st708611@gmail.com>
2025-03-14 15:57:13 +0545

    errorMiddlware problem solved

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -10,7 +10,7 @@ export const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
 
     if (err.code === 11000) {
-        const message = `Duplicate ${Object.keys(err.KeyValue)} Entered`;
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
         err = new ErrorHandler(message, 400);
     }
     if (err.name === "JsonWebTokenError") {
