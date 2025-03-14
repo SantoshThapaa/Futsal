9bf31800ba70c554f5517e595628f88fc19ff7e3
SantoshThapaa <st708611@gmail.com>
2025-03-14 15:57:45 +0545

    User route added

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import cors from "cors";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./dbConnection.js";
 import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js"
+
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -29,6 +31,7 @@ app.use(
     })
 );
 app.use("/api/v1/message",messageRouter);
+app.use("/api/v1/user", userRouter);
 dbConnection();
 
 app.use(errorMiddleware);
