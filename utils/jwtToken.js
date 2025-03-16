773aa75929e38e3b91df916499126bddcd2e9161
SantoshThapaa <st708611@gmail.com>
2025-03-16 12:21:24 +0545

    Jwt token created

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,12 +1,19 @@
-export const generateToken = (user, message, statusCode, res)=>{
+export const generateToken = (user, message, statusCode, res) => {
     const token = user.generateJsonWebToken();
-    const cookieName = user.role === 'admin' ? 'adminToken' : 'userToken';
-    res.ststus(statusCode).cookie(cookieName, token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-    }).json({
+    const cookieName = user.role === 'Admin' ? 'adminToken' : 'userToken';
+  
+    res
+      .status(statusCode)
+      .cookie(cookieName, token, {
+        expires: new Date(
+          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+      })
+      .json({
         success: true,
         message,
         user,
         token,
-    });
-};
+      });
+  };
