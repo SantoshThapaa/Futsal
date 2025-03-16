48ec435a25f396ac700128dff18af728bc9bb394
SantoshThapaa <st708611@gmail.com>
2025-03-16 13:28:14 +0545

    user Router modified

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { addNewAdmin, login, Register } from "../controller/userController.js";
-
+import { addNewAdmin, getAllCourts, getUserDetails, login, Register } from "../controller/userController.js";
+import { isAdminAuthenticated, isUserAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/client/register",Register);
 router.post("/login",login);
-router.post("/admin/addnew",addNewAdmin);
+router.post("/admin/addnew",isAdminAuthenticated, addNewAdmin);
+router.get("/courts", getAllCourts);
+router.get("/admin/me", isAdminAuthenticated, getUserDetails);
+router.get("/user/me", isUserAuthenticated, getUserDetails);
 
 export default router; 
