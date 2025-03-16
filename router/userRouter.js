683acb51fd50560b0fb6ec7e77117250f4914765
SantoshThapaa <st708611@gmail.com>
2025-03-16 12:23:00 +0545

    Routing added

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, Register } from "../controller/userController.js";
+import { addNewAdmin, login, Register } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/client/register",Register);
 router.post("/login",login);
+router.post("/admin/addnew",addNewAdmin);
 
 export default router; 
