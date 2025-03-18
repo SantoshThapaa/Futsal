b1febeb0ee2b9f3104c78d2f6520c6f1b7e21efe
SantoshThapaa <st708611@gmail.com>
2025-03-18 12:37:49 +0545

    Hero section deone

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Hero = () => {
-  return (
-    <div></div>
-  )
+const Hero = ({ title, imageUrl, width = "500px", height = "700px" }) => {
+    return (
+        <div className='hero container'>
+            <div className="banner">
+                <h1>{title}</h1>
+                <p>Book futsal courts instantly, manage your team, join tournaments, and track scores all in one place! Real time availability, easy payments, and hassle free scheduling make your futsal experience smooth and exciting. Ready to play? Let’s kick off!</p>
+            </div>
+            <div className="banner">
+                <img src={imageUrl} alt="hero" className="animated-image" style={{ width, height, objectFit: "contain" }}/>
+                <span>
+                    <img src="/Vector.png" alt="vector" />
+                </span>
+            </div>
+        </div>
+
+    )
 }
 
 export default Hero
