3ddd356ce921fd6f002e3ddc3ea667c2ce755024
SantoshThapaa <st708611@gmail.com>
2025-03-18 12:38:55 +0545

    Added

--- frontend/components/Biography.jsx
+++ frontend/components/Biography.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Biography = () => {
   return (
-    <div></div>
+    <div className='container biography'>
+        <div className='banner'></div>
+    </div>
   )
 }
 
