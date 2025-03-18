05b2a00f8ecf4506d781d130f3ab6ea83cf36cd1
SantoshThapaa <st708611@gmail.com>
2025-03-18 12:38:24 +0545

    All imported in Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../../components/Hero';
+import Biography from '../../components/Biography';
+import Location from '../../components/Location';
+import MessageForm from '../../components/MessageForm';
 
 const Home = () => {
-  return (
-    <>
-    <Hero/>
-    <Biography/>
-    <Location/>
-    <MessageForm/>
-    </>
-  )
+    return (
+        <>
+            <Hero title={"Welcome to FutZone Website | Your ultimate futsal booking hub!"} imageUrl={"/football.png"} />
+            <Biography />
+            <Location />
+            <MessageForm />
+        </>
+    )
 }
 
 export default Home
