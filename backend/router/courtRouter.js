9d1fd209cef8e9c054ea9180e2d15a929362aa1c
SantoshThapaa <st708611@gmail.com>
2025-03-20 14:53:19 +0545

    Modified

--- backend/router/courtRouter.js
+++ backend/router/courtRouter.js
@@ -7,6 +7,9 @@ import {
     deleteCourt,
     blockCourtSlot,
     bookCourt,
+    updateBookingStatus,
+    getAllBookings,
+    deleteBooking,
 } from "../controller/courtController.js";
 import { isAdminAuthenticated, isUserAuthenticated } from "../middlewares/auth.js";
 import { checkMaintenanceMode } from "../middlewares/maintenanceMiddleware.js";
@@ -19,39 +22,33 @@ router.post(
     isAdminAuthenticated, 
     addNewCourt
 );
-
-
 router.get("/all", getAllCourts);
-
-
 router.get("/:id", getCourtById);
-
-
 router.put(
     "/update/:id",
     isAdminAuthenticated, 
     updateCourt
 );
-
-
 router.delete(
     "/delete/:id",
     isAdminAuthenticated, 
     deleteCourt
 );
-
-
 router.post(
     "/block-slot/:id",
     isAdminAuthenticated, 
     blockCourtSlot
 );
-
 router.post(
     "/:courtId/book",
     checkMaintenanceMode, 
     isUserAuthenticated, 
     bookCourt
 );
+router.get("/:courtId/bookings", isUserAuthenticated, getAllBookings);
+router.put("/:courtId/bookings/:bookingId", isAdminAuthenticated, updateBookingStatus);
+router.delete("/:courtId/bookings/:bookingId", isAdminAuthenticated, deleteBooking);
+
+
 
 export default router;
