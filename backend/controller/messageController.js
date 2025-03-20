374dfabdfa78230e5f8cadd337142286f2939658
SantoshThapaa <st708611@gmail.com>
2025-03-20 14:52:46 +0545

    Get all messages made

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,6 +1,6 @@
 import { catchAsyncErrors } from '../middlewares/catchAsyncErrors.js'
+import ErrorHandler from '../middlewares/errorMiddleware.js';
 import { Message } from "../models/messageSchema.js";
-import ErrorHandler from "../middlewares/errorMiddleware.js";
 
 export const sendMessage =catchAsyncErrors(async (req, res, next) => {
     const { firstName, lastName, email, phone, message } = req.body;
@@ -12,4 +12,11 @@ export const sendMessage =catchAsyncErrors(async (req, res, next) => {
         success: true,
         message: "Message sent successfully!",
     });
-}) 
+});
+export const getAllMessages = catchAsyncErrors(async (req, res, next) => {
+    const messages = await Message.find();
+    res.status(200).json({
+      success: true,
+      messages,
+    });
+  });
